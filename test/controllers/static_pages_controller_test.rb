f9031453b4859c9f9bcad4a8d273968d4347f239
Koumarin <alicefigueiredo837@gmail.com>
2023-09-11 01:22:47 -0300

    refactor link tests to use full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get root_path
     assert_response :success
-    assert_select 'title', 'Magic Deckbuilder'
+    assert_select 'title', full_title
   end
 
   test 'should get help' do
     get help_path
     assert_response :success
-    assert_select 'title', 'Help | Magic Deckbuilder'
+    assert_select 'title', (full_title 'Help')
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', 'About | Magic Deckbuilder'
+    assert_select 'title', (full_title 'About')
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', 'Contact | Magic Deckbuilder'
+    assert_select 'title', (full_title 'Contact')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 require 'minitest/reporters'
+require 'test_helper'
 
 Minitest::Reporters.use!
 
@@ -12,5 +13,5 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
