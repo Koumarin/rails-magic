30bd39340f4b2ca7842f3bd5a16ca37eb4c4c4f9
Koumarin <alicefigueiredo837@gmail.com>
2023-09-11 22:57:02 -0300

    add email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = 'a' * 244 + '@example.com' # Should have at most 255 chars.
     assert_not @user.valid?
   end
+
+  test 'email validation should accept valid addresses' do
+    addresses = %w[user@example.com  USER@foo.COM     A_US-ER@foo.bar.org
+                   first.last@foo.jp alice+bob@baz.cn]
+    addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    addresses = %w[user@example,com user_at_foo.org user.name@example.
+                   foo@bar_baz.com  foo@bar+baz.com user@example..com]
+    addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
 end
