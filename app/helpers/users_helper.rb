e3ce217b5a87aae27e279b5426814121b5569ab6
Koumarin <alicefigueiredo837@gmail.com>
2023-09-15 02:54:02 -0300

    add gravatar to user page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  ### Returns the gravatar for the given user.
+  def gravatar_for user, size: 80
+    id  = Digest::MD5::hexdigest user.email.downcase
+    url = "https://secure.gravatar.com/avatar/#{id}"
+
+    image_tag url, alt: user.name, class: 'gravatar', size: size
+  end
 end
