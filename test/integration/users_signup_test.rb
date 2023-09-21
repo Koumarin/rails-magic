ff7c6c79f51257f227eaaaa625bc91fdc2f76ba8
Koumarin <alicefigueiredo837@gmail.com>
2023-09-21 03:55:48 -0300

    add valid signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation' # The error messages should be
     assert_select 'div.alert-danger'      # displayed in the page.
   end
+
+  test 'valid signup information' do
+    assert_difference 'User.count', 1 do
+      post users_path,
+           params: {user: {name:                  'user',
+                           email:                 'user@example.com',
+                           password:              'password',
+                           password_confirmation: 'password'}}
+    end
+
+    follow_redirect!                    # We should be redirected
+    assert_template 'users/show'        # to the new user's page.
+    assert_select 'div.alert-success'   # There should be a success message.
+  end
 end
